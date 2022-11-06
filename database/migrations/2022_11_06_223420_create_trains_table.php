19e2817567e083f92548b2f0aa09520a88d4f9bf
PietroDanieli <1pie7d@gmail.com>
2022-11-07 00:34:53 +0100

    create table 'trains'

--- database/migrations/2022_11_06_223420_create_trains_table.php
+++ database/migrations/2022_11_06_223420_create_trains_table.php
@@ -11,10 +11,32 @@ class CreateTrainsTable extends Migration
      *
      * @return void
      */
+
+    // Azienda
+    // Stazione di partenza
+    // Stazione di arrivo
+    // Orario di partenza
+    // Orario di arrivo
+    // Codice Treno
+    // Numero Carrozze
+    // In orario
+    // Cancellato
+
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 150);  //tabella?
+            $table->string('Stazione_di_partenza', 150);  //tabella?
+            $table->string('Stazione_di_arrivo', 150);  //tabella?
+            $table->time('Orario_di_partenza');
+            $table->time('Orario_di_arrivo');
+            $table->date('Data_di_partenza');
+            $table->date('Data_di_arrivo');
+            $table->string('Codice_Treno', 10)->unique();
+            $table->tinyInteger('Numero_Carrozze')->unsigned()->default(0);
+            $table->boolean('In_orario')->default(true);
+            $table->tinyInteger('cancellato')->default(0);
             $table->timestamps();
         });
     }
