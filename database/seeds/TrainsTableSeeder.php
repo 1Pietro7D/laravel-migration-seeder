130fae85eec5f29f22875a4b2c29711b53464d03
PietroDanieli <1pie7d@gmail.com>
2022-11-07 01:46:04 +0100

    create seeder and compiled

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
+use App\Models\Train;
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -9,8 +10,23 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
-    }
+        for($i = 0; $i < 25; $i++){
+        $randomDate= $faker->date();
+
+        $train = new Train();
+        $train->Azienda= $faker->name();
+        $train->Stazione_di_partenza= $faker->city();
+        $train->Stazione_di_arrivo= $faker->city();
+        $train->Orario_di_partenza= $faker->time();
+        $train->Orario_di_arrivo= $faker->time();
+        $train->Data_di_partenza= $randomDate;
+        $train->Data_di_arrivo= $randomDate;
+        $train->Codice_Treno= $faker-> randomLetter() . $i;
+
+
+        $train->save();
+    }}
 }
